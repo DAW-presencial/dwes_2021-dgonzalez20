47b8465d581e93131bc9bacaa3545858337cbee6
david <dgonzalezl@cifpfbmoll.eu>
2021-12-03 10:16:15 +0100

    feat:Actualización prácticas

--- Prácticas/ComprobarFichero.php
+++ Prácticas/ComprobarFichero.php
@@ -1,5 +1,5 @@
 <?php
-$archivo=$_FILES['SubirFichero']['temp_name'];
+$archivo=$_FILES['SubirFichero[]g']['temp_name'];
 $extemsion=pathinfo($archivo,PATHINFO_EXTENSION);
  if (move_uploaded_file($archivo,"nuevoarchivo.pdf".$extemsion));
  echo "El fichero se ha subido correctamente";
