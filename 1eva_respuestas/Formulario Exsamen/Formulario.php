c05da4c4f74bdf75433f8328ed968086ecfe4630
david <dgonzalezl@cifpfbmoll.eu>
2021-12-03 16:24:34 +0100

    Feat: Creo formulario.php completo

---
+++ 1eva_respuestas/Formulario Exsamen/Formulario.php
@@ -0,0 +1,29 @@
+<html xmlns="http://www.w3.org/1999/html">
+<head>Formulario Exsamen</head>
+<body>
+<form action="" method="POST" ENCTYPE="multipart/form-data"
+<label> Nombre</label>
+<input type="text" name="nombre" id="nombre">
+
+<label> Apellido </label>
+<input type="text" name="apellido" id="apellido">
+
+<label> Fecha de nacimiento</label>
+<input type="datetime-local" name="nacimiento">
+
+<!-- Control fichero 1-->
+<label Fichero 1</label>
+<input type="file" name="fichero_1" id="fichero1">
+
+<!-- Control Fichero 2-->
+<label Fichero 2 </label>
+<input type="file" name="fichreo_2" id="fichero2">
+
+<input type="submit" name="enviar" id="enviar">Enviar 
+
+</body>
+
+</html>
+
+<?php
+?>
